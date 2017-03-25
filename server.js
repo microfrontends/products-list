979d2886a53cdc9ebc1baebe21ea616aa501dda6
Rogerio Chaves <rogeriocfj@gmail.com>
2017-03-25 18:22:40 -0300

    Change name of the root div

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ server.get('/', (req, res) => {
   const htmlPath = path.resolve(__dirname, 'build', 'index.html');
 
   fs.readFile(htmlPath, 'utf8', (err, html) => {
-    const rootElem = '<div id="root">';
+    const rootElem = '<div id="products-list-root">';
     const renderedApp = renderToString(React.createElement(App, null));
 
     setTimeout(() => {
