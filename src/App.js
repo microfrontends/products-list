211c6a7b008e8484dcb0a30810759ed900f35d1a
Rogerio Chaves <rogeriocfj@gmail.com>
2017-03-27 21:51:26 -0300

    Add event to add to cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const productItemView = (product) =>
-  <li style={{ padding: "30px", listStyle: "none", border: "1px solid #000", margin: "5px" }}>
-    <b>{product}</b> <br />
-    $ {(Math.random() * 100).toFixed(2)} <br />
-    <button>Buy</button>
-  </li>
+const addToCart = (item) => () => {
+  const event = new CustomEvent('addToCart', { detail: item });
+  window.dispatchEvent(event);
+}
+
+const productItemView = (product, index) => {
+  const price = parseFloat((Math.random() * 100).toFixed(2));
+
+  return (
+    <li key={index} style={{ padding: "30px", listStyle: "none", border: "1px solid #000", margin: "5px" }}>
+      <b>{product}</b> <br />
+      $ {price} <br />
+      <button onClick={addToCart({ product, price })}>Buy</button>
+    </li>
+  );
+};
 
 export default () =>
   <section>
